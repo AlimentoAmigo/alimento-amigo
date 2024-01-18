d69408650535083aeade22a7ca354c36aa459b1a
André Prado Junior <116444319+Andrepjr@users.noreply.github.com>
2024-01-18 15:17:52 -0300

    Corrigindo erros

---
+++ alimentoamigo/src/main/java/com/generation/alimentoamigo/repository/CategoriaRepository.java
@@ -0,0 +1,16 @@
+package com.generation.alimentoamigo.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.generation.alimentoamigo.model.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+	List<Categoria> findAllByTipoContainingIgnoreCase(@Param("tipo") String tipo);
+	
+}
